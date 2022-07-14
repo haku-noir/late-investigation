101c4ed9050a9653998e7a532f9c670fb26f38ee
haku-noir <hakunoir.00@gmail.com>
2022-07-14 19:37:32 +0900

    add: edit user function

--- app/late_investigation/forms.py
+++ app/late_investigation/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from .models import CustomUser
 
 # フォームクラス作成
@@ -13,3 +15,52 @@ class CustomUserForm(forms.ModelForm):
         fields = ('last_name','first_name','username','email','password',)
         # フィールド名指定
         labels = {'last_name':"名字",'first_name':"名前",'username':"ユーザー名",'email':"メールアドレス"}
+
+    # def clean_email(self):
+    #     email = self.cleaned_data["email"]
+    #     try:
+    #         validate_email(email)
+    #     except ValidationError:
+    #         raise ValidationError("正しいメールアドレスを指定して下さい。")
+
+    #     try:
+    #         CustomUser.objects.filter(email=email).order_by("-id").first()
+    #     except CustomUser.DoesNotExist:
+    #         return email
+    #     else:
+    #         raise ValidationError("このメールアドレスは既に使用されています。別のメールアドレスを指定してください")
+
+class CustomUserEditForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30,required=True)
+    last_name = forms.CharField(max_length=30,required=True)
+    # パスワード入力：非表示対応
+    password = forms.CharField(widget=forms.PasswordInput(),label="パスワード")
+
+    def __init__(self, user, *args, **kwargs):
+        self.login_user = user
+        super().__init__(*args, **kwargs)
+
+    class Meta():
+        # ユーザー認証
+        model = CustomUser
+        # フィールド指定
+        fields = ('last_name','first_name','number','username','email','password',)
+        # フィールド名指定
+        labels = {'last_name':"名字",'first_name':"名前", 'number':"学生番号",'username':"ユーザー名",'email':"メールアドレス"}
+
+    # def clean_email(self):
+    #     email = self.cleaned_data["email"]
+    #     try:
+    #         validate_email(email)
+    #     except ValidationError:
+    #         raise ValidationError("正しいメールアドレスを指定して下さい。")
+
+    #     try:
+    #         CustomUser.objects.filter(email=email).order_by("-id").first()
+    #     except CustomUser.DoesNotExist:
+    #         return email
+    #     else:
+    #         if self.login_user.email == email:
+    #             return email
+    #         raise ValidationError("このメールアドレスは既に使用されています。別のメールアドレスを指定してください")
--- app/late_investigation/views.py
+++ app/late_investigation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView # テンプレートタグ
-from .forms import CustomUserForm # ユーザーアカウントフォーム
+from .forms import CustomUserForm, CustomUserEditForm # ユーザーアカウントフォーム
+from .models import CustomUser
 from django.contrib.auth import authenticate
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
@@ -8,7 +9,7 @@ from django.urls import reverse
 def home(request):
     return render(request, 'home.html')
 
-class AccountRegistration(TemplateView):
+class UserRegister(TemplateView):
 
     def __init__(self):
         self.params = {
@@ -43,3 +44,45 @@ class AccountRegistration(TemplateView):
             print(self.params["custom_user_form"].errors)
 
         return render(request, "registration/register.html", context=self.params)
+
+
+class UserEdit(TemplateView):
+
+    def __init__(self):
+        self.params = {
+            "AccountChange": False,
+            "custom_user_edit_form": CustomUserEditForm(user=None),
+        }
+
+    # Get処理
+    def get(self,request):
+        user = request.user
+        self.params["custom_user_edit_form"] = CustomUserEditForm(user=user, instance=user)
+        self.params["AccountChange"] = False
+        return render(request,"users/edit.html", context=self.params)
+
+    # Post処理
+    def post(self,request):
+        user = request.user
+        self.params["custom_user_edit_form"] = CustomUserEditForm(user=user, instance=user, data=request.POST)
+
+        # フォーム入力の有効検証
+        if self.params["custom_user_edit_form"].is_valid():
+            # アカウント情報をDB保存
+            custom_user = self.params["custom_user_edit_form"].save()
+            # パスワードをハッシュ化
+            custom_user.set_password(custom_user.password)
+            # ハッシュ化パスワード更新
+            custom_user.save()
+
+            # アカウント作成情報更新
+            self.params["AccountChange"] = True
+
+        else:
+            # フォームが有効でない場合
+            print(self.params["custom_user_edit_form"].errors)
+
+        return render(request, "users/edit.html", context=self.params)
+
+    def get_object(self):
+        return self.request.user
