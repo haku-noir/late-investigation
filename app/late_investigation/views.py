fafb4598e3f7acc8addad182d29cfda0f92bd284
haku-noir <hakunoir.00@gmail.com>
2022-07-15 12:59:39 +0900

    add: register route function

--- app/late_investigation/views.py
+++ app/late_investigation/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView # テンプレートタグ
-from .forms import CustomUserForm, CustomUserEditForm # ユーザーアカウントフォーム
-from .models import CustomUser
+from .forms import CustomUserForm, CustomUserEditForm, RouteForm # ユーザーアカウントフォーム
+from .models import CustomUser, Route
 from django.contrib.auth import authenticate
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
@@ -88,3 +88,33 @@ class UserEdit(TemplateView):
 
     def get_object(self):
         return self.request.user
+
+class Routelist(TemplateView):
+
+    def __init__(self):
+        self.params = {
+            "route_form": RouteForm(),
+            "routes": Route.objects.all()
+        }
+
+    # Get処理
+    def get(self,request):
+        user = request.user
+        self.params["route_form"] = RouteForm()
+        self.params["routes"] = Route.objects.all()
+        return render(request,"routes/list.html", context=self.params)
+
+    # Post処理
+    def post(self,request):
+        self.params["route_form"] = RouteForm(data=request.POST)
+
+        # フォーム入力の有効検証
+        if self.params["route_form"].is_valid():
+            # 路線情報をDB保存
+            self.params["route_form"].save()
+
+        else:
+            # フォームが有効でない場合
+            print(self.params["route_form"].errors)
+
+        return render(request, "routes/list.html", context=self.params)
